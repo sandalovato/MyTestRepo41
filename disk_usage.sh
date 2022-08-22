f8fafe7effe97bc3fa0e05fdd5e06033390ffd1d
sandalovato <sandalovato@gmail.com>
2022-08-22 11:32:16 +0300

    default

--- disk_usage.sh
+++ disk_usage.sh
@@ -4,5 +4,9 @@ if [-d/ $dir]; then
 	echo -e "checking disk usage in $dir\n"
 else
 	echo -e "$dir is not a directory"
-	exit 1
+	exit 101
 fi
+echo "heaviest folder in $dir in: "
+top1=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 1 | \tail - n 1|cut -f 2)
+disk=$(du /home/pyprep02 -d 1 2>/dev/null | sort -rn | head -n 1 | \tail - n 1|cut -f 1)
+echo -e "$top1 with disk usage of $disk"
